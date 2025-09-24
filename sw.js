2b2800a77eeae3df471a6e5ddfa0e972e845f8be
CodeWithSahil <fair29565@gmail.com>
2025-09-24 16:56:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: 'https://i.pinimg.com/736x/ef/c8/25/efc8255e3dc062530d2153b5fe807732.jpg'
-  });
-});
+ 
